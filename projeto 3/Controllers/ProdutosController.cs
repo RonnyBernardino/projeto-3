296c8c6e69ab2c8a20a4a49ecd9e423643bf2f16
0220482113036 <0220482113036@LAB4TC24>
2022-02-25 21:05:26 -0300

    Adicionar arquivos de projeto.

---
+++ projeto 3/Controllers/ProdutosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projeto_3.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult cadastro()
+        {
+            return View();
+        }
+    }
+}
---
+++ projeto 3/Models/ProdutoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projeto_3.Models
+{
+    public class ProdutoModel
+    {
+        public int Codigo { get; set; }
+
+        public decimal Valor { get; set; }
+        public string Nome { get; set; }
+
+    }  
+}
